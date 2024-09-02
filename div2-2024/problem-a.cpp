6147af2e1fef386963edbb92956e962eb57f0ba8
gustavo <lgmamaniap@gmail.com>
2024-09-02 16:36:05 -0400

    feat: :sparkles: solution for problem a

---
+++ div2-2024/problem-a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  for (int i=0; i<t; i++) {
+    int n;
+    cin >> n;
+    if (n > 9) {
+      cout << 9 << endl;
+      continue;
+    }
+
+    cout << n << endl;
+  }
+
+  return 0;
+}
